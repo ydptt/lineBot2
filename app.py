7517e9f270b97a4a55a0b4997ade00f858001350
ydptt <56101582+ydptt@users.noreply.github.com>
2019-10-03 10:17:07 +0700

    Update app.py

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ def callback():
     json_line = request.get_json()
     json_line = json.dumps(json_line)
     decoded = json.loads(json_line)
-    user = decoded["events"][0]['replyToken']
+    user = decoded['originalDetectlntentRequest']['payload']['data']['replyToken']
     userText = decoded["events"][0]['message']['text']
     sendText(user,userText)
     return '',200
